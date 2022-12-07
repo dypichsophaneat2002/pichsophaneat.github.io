cbaa754c8feaa65b13949d0ffc3dbb01c76d3551
Neat <sophaneat@student.fdu.edu>
2022-12-06 21:57:02 -0800

    Add dark mode

---
+++ index.js
@@ -0,0 +1,24 @@
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2")
+const body = document.body;
+const item1 = document.getElementById("item1");
+const item2 = document.getElementById("item2");
+const item3 = document.getElementById("item3");
+button1.addEventListener("click", function(){
+    if (body.style.backgroundColor == "white"){
+        body.style.backgroundColor = "black";
+        item1.style.color = "white";
+        item2.style.color = "white";
+        item3.style.color = "white";
+        button1.setAttribute("class", "btn btn-outline-light my-5");
+        button2.setAttribute("class", "btn btn-outline-light");
+    } else {
+        body.style.backgroundColor = "white";
+        item1.style.color = "black";
+        item2.style.color = "black";
+        item3.style.color = "black";
+        button1.setAttribute("class", "btn btn-outline-dark my-5");
+        button2.setAttribute("class", "btn btn-outline-dark");
+    }
+        
+})
