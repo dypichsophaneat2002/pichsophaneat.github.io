c29b1e1817dc099c23754fab9a83b8bc6596d955
Neat <sophaneat@student.fdu.edu>
2022-12-09 09:39:56 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const item1 = document.getElementById("item1");
 const item2 = document.getElementById("item2");
 const item3 = document.getElementById("item3");
 button1.addEventListener("click", function(){
-    if (mode){
+    if (mode == true){
         body.style.backgroundColor = "black";
         item1.style.color = "white";
         item2.style.color = "white";
         item3.style.color = "white";
         button1.setAttribute("class", "btn btn-outline-light my-5");
         button2.setAttribute("class", "btn btn-outline-light");
-    } else {
+        mode = false;
+    } else if (mode == false) {
         body.style.backgroundColor = "white";
         item1.style.color = "black";
         item2.style.color = "black";
